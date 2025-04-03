53e2a0b1cb56ce07f2b378c940501a322deeed09
RITVIKRAMACHANDRAN <142288694+RITVIKRAMACHANDRAN@users.noreply.github.com>
2025-04-03 20:19:47 +0530

    EVOTING

--- server/server.js
+++ server/server.js
@@ -207,5 +207,8 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(buildPath, "index.html"));
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
